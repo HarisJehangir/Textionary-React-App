8b2264b57fcaa95e07e1be6973df0192489c9937
HarisJehangir <58706769+HarisJehangir@users.noreply.github.com>
2020-12-02 17:28:19 +0200

    Links
    Added Social media links to footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,6 +7,25 @@ function Footer() {
   return (
     <div>
       <footer>
+        <a
+          className="footer-link"
+          href="https://www.linkedin.com/in/haris-jehangir-9721ab17b/"
+        >
+          LinkedIn
+        </a>
+        <a
+          className="footer-link"
+          href="https://harisjehangir.github.io/HJ-Personal-Site/"
+        >
+          CV
+        </a>
+        <a
+          className="footer-link"
+          href="https://www.facebook.com/MessiIsLove?ref=bookmarks"
+        >
+          Facebook
+        </a>
+
         <p> Haris Jehangir © {year}. All Rights reserved.</p>
       </footer>
     </div>
